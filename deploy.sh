91e74637ffd1a11a7441a326038dd07f518cba51
Jakub <jakub.karkosza@nettmaker.no>
2022-01-26 16:48:31 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:JKarkosza/battle-tracker.git main:gh-pages
+git push -f https://github.com/JKarkosza/battle-tracker.git main:gh-pages
 cd -
